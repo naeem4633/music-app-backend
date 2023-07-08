1820a199a89c860e0eb18fdc507d6e04852ceb8a
naeem4633 <ahmedn3700@gmail.com>
2023-07-08 17:46:58 +0500

    Added a view to display the current song

--- spotify/views.py
+++ spotify/views.py
@@ -6,7 +6,7 @@ from rest_framework import status
 from requests import Request, post
 from .models import SpotifyToken
 from django.shortcuts import redirect
-from .utils import get_user_tokens, is_spotify_authenticated
+from .utils import get_user_tokens, is_spotify_authenticated, refresh_spotify_token, execute_spotify_api_request
 from datetime import datetime, timedelta
 from django.utils import timezone
 
@@ -26,7 +26,6 @@ class AuthURL(APIView):
 
 def spotify_callback(request, format=None):
 
-    print("callback function called")
     code = request.GET.get('code')
     print("code from url: " + code)
     error = request.GET.get('error')
@@ -45,10 +44,9 @@ def spotify_callback(request, format=None):
     expires_at = timezone.now() + timedelta(seconds=int(expires_in))
 
     error = response.get('error')
-    print("session_key in spotify callback: " + str(request.session.session_key))
+    # print("session_key in spotify callback: " + str(request.session.session_key))
     if not request.session.exists(request.session.session_key):
         request.session.create()
-        print("session_key in spotify callback after session.create(): " + str(request.session.session_key))
         SpotifyToken.objects.update_or_create(
             user=request.session.session_key,
             defaults={
@@ -63,7 +61,44 @@ def spotify_callback(request, format=None):
 
 class IsAuthenticated(APIView):
     def get(self, request, format=None):
-        print("isAuthenticated get function called")
-        print("session_key in isAuth: " + str(self.request.session.session_key))
+        # print("isAuthenticated get function called")
+        # print("session_key in isAuth: " + str(self.request.session.session_key))
         is_authenticated = is_spotify_authenticated(self.request.session.session_key)
         return Response({'status': is_authenticated}, status=status.HTTP_200_OK)
+
+class CurrentSong(APIView):
+    def get(self, request, format=None):
+        session_id = self.request.session.session_key
+        endpoint = "player/currently-playing"
+        response = execute_spotify_api_request(session_id, endpoint)
+
+        if 'error' in response or 'item' not in response:
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+        item = response.get('item')
+        duration = item.get('duration_ms')
+        progress = response.get('progress_ms')
+        album_cover = item.get('album').get('images')[0].get('url')
+        is_playing = response.get('is_playing')
+        song_id = item.get('id')
+
+        artist_string = ""
+
+        for i, artist in enumerate(item.get('artists')):
+            if i > 0:
+                artist_string += ", "
+            name = artist.get('name')
+            artist_string += name
+
+        song = {
+            'title': item.get('name'),
+            'artist': artist_string,
+            'duration': duration,
+            'progress': progress,
+            'image_url': album_cover,
+            'is_playing': is_playing,
+            'votes': 0,
+            'id': song_id
+        }
+
+        return Response(song, status=status.HTTP_200_OK)
